0a240845ef1a8548e535db3e734f2b6b43ac3dfe
Arjun Sharma <arjun0941.becse24@chitkara.edu.in>
2026-06-01 20:44:19 +0530

    oops problem

---
+++ stringoops.java
@@ -0,0 +1,16 @@
+public class stringoops {
+    public static void main(String[] args) {
+        // String name = new String("Arjun");
+
+        // System.out.println("hello " + name);
+
+        String name = "Arjun ";
+        name = name + "Sharma";
+        System.out.println(name);
+
+        String s1 = "Arjun";
+        String s2 = "Arjun";
+
+        
+    }
+}
