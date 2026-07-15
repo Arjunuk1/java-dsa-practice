49a173fa76d6641a8002af0817cdf39e9617a153
Arjun Sharma <arjun0941.becse24@chitkara.edu.in>
2026-07-16 00:27:36 +0530

    Solved Divide Two Integers using Bit Manipulation

---
+++ DivideTwoIntegers.java
@@ -0,0 +1,10 @@
+public class DivideTwoIntegers {
+        public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        
+        int divide = dividend / divisor;
+        return divide;
+    }
+}
