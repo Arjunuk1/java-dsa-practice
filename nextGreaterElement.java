4799a2c9c9976fffe2789b4aa60473e3a05d11e3
Arjun Sharma <arjun0941.becse24@chitkara.edu.in>
2026-09-05 19:06:05 +0530

    Improved Next Greater Element solution using Stack

---
+++ nextGreaterElement.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+public class nextGreaterElement {
+        public ArrayList<Integer> nextLargerElement(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                result.add(-1);
+            } else {
+                result.add(stack.peek());
+            }
+
+            stack.push(arr[i]);
+        }
+        Collections.reverse(result);
+        return result;
+    }
+}
