b343e779c27f5ebcda6d25db3aeec4fded2dab8f
Arjun Sharma <arjun0941.becse24@chitkara.edu.in>
2026-09-06 22:06:37 +0530

    Practiced calculating the product of array elements

---
+++ productOfAll.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class productOfAll {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = scn.nextInt();
+        }
+
+        int prod = 1;
+
+        for(int i = 0;i<n;i++){
+            prod *= arr[i];
+        }
+
+        System.out.println(prod);
+    }
+}
