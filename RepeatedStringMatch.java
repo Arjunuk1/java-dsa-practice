bae7dc1aff95836ffb1587534905e6a29777d12a
Arjun Sharma <arjun0941.becse24@chitkara.edu.in>
2026-09-14 20:41:51 +0530

    Solved leetcode Repeated String Match

---
+++ RepeatedStringMatch.java
@@ -0,0 +1,25 @@
+public class RepeatedStringMatch {
+        public int repeatedStringMatch(String a, String b) {
+        
+        StringBuilder str = new StringBuilder();
+        int count = 0;
+        
+        while (str.length() < b.length()) {
+            str.append(a);
+            count++;
+        }
+        
+        if (str.toString().contains(b)) {
+            return count;
+        }
+        
+        str.append(a);
+        count++;
+        
+        if (str.toString().contains(b)) {
+            return count;
+        }
+        
+        return -1;
+    }
+}
